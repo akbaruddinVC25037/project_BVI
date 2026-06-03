1a0bd781eea14a3f63bead06d517de929449e23d
akbaruddinVC25037 <vc25037@adab.umpsa.edu.my>
2026-06-03 17:18:29 +0800

    Add files via upload

---
+++ LAB 3.py
@@ -0,0 +1,31 @@
+'''count = 0
+while (count < 10):
+ count += 1
+ print("this is loop",count)'''
+
+
+count = 0
+say = ""
+txt = ""
+print("If you want to stop, type 's' and 'S'")
+
+while txt != "s" and txt != "S":
+  
+ txt = input("Enter some text: ")
+ count += 1 
+ say += txt
+print("You have stopped the input.")
+print("Total input count: ", count)
+print("You said: ", say)
+
+
+'''num = int(input("Enter number: "))
+for i in range (num,0,-1 ):
+ print(i)
+ print("blast off!")
+
+
+for i in range (27,3,-3 ):
+ print(i) '''   
+
+
